5cc11416a3e33569318b56562b7d0b9d5e5aa547
eternitypain <eternaldespair7474@gmail.com>
2024-07-27 21:56:29 +0300

    Added dark theme and fix header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 import '../assets/scss/main.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+
+
+
+useTheme();
+useBurger();
