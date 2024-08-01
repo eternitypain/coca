72e23068fb085f040650218d0bbcd1546e8dc8b2
eternitypain <eternaldespair7474@gmail.com>
2024-08-01 21:52:43 +0300

    returned map

--- src/js/contact.js
+++ src/js/contact.js
@@ -6,7 +6,7 @@ import '/scss/contact.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useHeader } from './components/header.js';
-// import { useMap } from './components/contact/contactMap.js';
+import { useMap } from './components/contact/contactMap.js';
 import { usePartnersSlider } from './components/slider.js';
 import { usePhone } from './components/contact/phone.js';
 import { useAccordionFooter } from './components/footer.js';
