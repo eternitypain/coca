dcfa9df10e74ee89248cf42e5a72b37d54c951b3
eternitypain <eternaldespair7474@gmail.com>
2024-08-02 03:16:35 +0300

    fix map new

--- eslint.config.js
+++ eslint.config.js
@@ -5,3 +5,7 @@ export default [
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
 ];
+
+module.exports = {
+    extends: ['./node_modules/@yandex/ymaps3-cli/.eslintrc.js'],
+};
